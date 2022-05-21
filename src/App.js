e39eaf617366909af60bcb648925341feef64dd8
John Hook <jhook1@ufl.edu>
2022-05-21 15:31:15 -0400

    Converted to components

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
 "use strict";
 
-const LayoutSep = React.createElement("hr",null,null);
+const e = React.createElement;
 
-const RootHeader = React.createElement(
-    "header",{className:"root-header"},React.createElement(
-        "h1",null,"Home"
-    ),React.createElement(
-        "nav",null,React.createElement(
-            "ul",null,React.createElement(
-                "li",null,React.createElement(
-                    "a",{href:"/index.html"},"Home"
+const LayoutSep = e("hr",null,null);
+
+class RootHeader extends React.Component {
+    render() {
+        return e(
+            "header",{className:"root-header"},e(
+                "h1",null,this.props.title
+            ),e(
+                "nav",null,e(
+                    "ul",null,e(
+                        "li",null,e(
+                            "a",{href:"/index.html"},"Home"
+                        )
+                    )
                 )
             )
-        )
-    )
-);
+        ); 
+    } 
+}
 
-const MainContent = React.createElement(
-    "main",null,React.createElement(
-        "p",null,"Hello World!"
-    )
-);
+class MainContent extends React.Component {
+    render() {
+        return e(
+            "main",null,e(
+                "p",null,"Hello World!"
+            )
+        );
+    }
+}
 
-const RootFooter = React.createElement(
-    "footer",{className:"root-footer"},React.createElement(
-        "p",null,"View Source: ",React.createElement(
-            "a",{href:"https:github.com/jhook1/jhook1.github.io"},"jhook1.github.io"
-        )," - ","Check out my other work: ",React.createElement(
-            "a",{href:"https:github.com/jhook1/"},"jhook1"
-        )
-    )
-);
+class RootFooter extends React.Component {
+    render() {
+        return e(
+            "footer",{className:"root-footer"},e(
+                "p",null,"View Source: ",e(
+                    "a",{href:"https:github.com/jhook1/jhook1.github.io"},"jhook1.github.io"
+                )," - ","Check out my other work: ",e(
+                    "a",{href:"https:github.com/jhook1/"},"jhook1"
+                )
+            )
+        );
+    }
+}
 
-const Doc = React.createElement(
-    "div",{className:"react-container"},
-    RootHeader,LayoutSep,MainContent,LayoutSep,RootFooter
-);
+class App extends React.Component {
+    render() {
+        return e(
+            "div",{className:"react-container"},
+            e(RootHeader,{title:"Home"},null),
+            LayoutSep,e(MainContent,null,null),LayoutSep,
+            e(RootFooter,null,null)
+        );
+    }
+}
 
 const domContainer = document.getElementById("react-container");
 const root = ReactDOM.createRoot(domContainer);
-root.render(Doc);
+root.render(e(App,null,null));
