fe51ec480ee371cb9093fe9c6ff089d85642157e
John Hook <jhook1@ufl.edu>
2022-05-22 00:10:19 -0400

    Experimenting w/ reading md text from file

--- src/Viewer.js
+++ src/Viewer.js
@@ -7,6 +7,7 @@ const writer = new commonmark.HtmlRenderer();
 export default function Viewer(props) {
     var parsed = reader.parse("# Hello **world**");
     var output = writer.render(parsed);
+
     const createMarkupFromString = (str) => {
         return {__html: str};
     }
