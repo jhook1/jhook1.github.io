f985f4879e86a89a4e911a7f275d53c73c3fcaf0
John Hook <jhook1@ufl.edu>
2022-05-22 11:08:50 -0400

    Additional styling for footer. Need to rework sticky header

--- src/Footer.js
+++ src/Footer.js
@@ -5,14 +5,12 @@ export default function Footer(props) {
     return e(
         "footer",{className:"root-footer"},e(Divider),e(
             Segment,null,e(
-                "p",{className:"footer-text"},e(
-                    "span",{className:"left-text"},"View Source: ",e(
+                "span",{className:"footer-text"},e(
+                    "p",{className:"left-text"},"View Source: ",e(
                         "a",{href:"https:github.com/jhook1/jhook1.github.io"},"jhook1.github.io"
                     )
-                ),e(
-                    "span",null,"-"
-                ),e(
-                    "span",{className:"right-text"},"Check out my other work: ",e(
+                ),e("hr"/* Divider,{className:"center-break"} */),e(
+                    "p",{className:"right-text"},"Check out my other work: ",e(
                         "a",{href:"https:github.com/jhook1/"},"jhook1"
                     )
                 )
