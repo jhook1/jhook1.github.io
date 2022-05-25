6172bfcef3e2a8fac5581c1d79b197709a0b06b9
John Hook <jhook1@ufl.edu>
2022-05-25 18:31:26 -0400

    Working on some new footer styling

--- src/Footer.js
+++ src/Footer.js
@@ -4,17 +4,17 @@ const {Segment,Label,Icon} = semanticUIReact;
 export default function Footer() {
     return e(
         Segment,{className:"page-footer",inverted:true,as:"footer"},e(
-            Label,{className:"label-icon-spacing",as:"a",href:"https://pages.github.com/"}
+            Label,{className:"label-icon-spacing footer-link",as:"a",href:"https://pages.github.com/"}
             ,"Hosted on Github Pages",e(
                 Icon,{name:"github",size:"large"}
             )
         ),e(
-            Label,{className:"label-icon-spacing",as:"a",href:"https://github.com/jhook1/jhook1.github.io/"}
+            Label,{className:"label-icon-spacing footer-link",as:"a",href:"https://github.com/jhook1/jhook1.github.io/"}
             ,"View Site Source",e(
                 Icon,{name:"github",size:"large"}
             )
         ),e(
-            Label,{className:"label-icon-spacing",as:"a",href:"https://reactjs.org/"}
+            Label,{className:"label-icon-spacing footer-link",as:"a",href:"https://reactjs.org/"}
             ,"Built with React",e(
                 Icon,{name:"react",size:"large",color:"blue"}
             )
